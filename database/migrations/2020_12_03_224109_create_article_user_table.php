8221ffd5c01be422b9b6be8617f1bc82c60dd2a6
Slava <test@mail.test>
2020-12-04 01:18:28 +0200

    refactor User & create many2many table

--- database/migrations/2020_12_03_224109_create_article_user_table.php
+++ database/migrations/2020_12_03_224109_create_article_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateArticleUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('article_user', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+
+            $table->unsignedInteger('user_id')->default(1);
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->unsignedInteger('article_id')->default(1);
+            $table->foreign('article_id')->references('id')->on('articles');
+
             $table->timestamps();
         });
     }
@@ -31,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('article_user');
     }
 }
